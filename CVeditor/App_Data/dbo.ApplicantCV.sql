2d191cff040957ff992e6fc728e9b94cab04bfdb
KristinaBarutska <kristina.barutska@abv.bg>
2018-06-25 23:48:53 +0300

    Refactoring made

--- CVeditor/App_Data/dbo.ApplicantCV.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] VARCHAR(50) NOT NULL PRIMARY KEY, 
-    [Details] VARCHAR(MAX) NOT NULL, 
-    [Education] VARCHAR(MAX) NOT NULL, 
-    [Employment] NVARCHAR(MAX) NOT NULL
-)
